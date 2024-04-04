cfc8477960672dfc7a90b9ba355c166cc4e5f188
Choi YoungLyn <joeuni0129@gmail.com>
2024-04-04 16:08:39 +0900

    [Feat] #248 - SizeLiterals 팝업 width 추가 및 alertType추가

--- Umbba-iOS/Umbba-iOS/Global/Literals/SizeLiterals.swift
+++ Umbba-iOS/Umbba-iOS/Global/Literals/SizeLiterals.swift
@@ -13,6 +13,7 @@ struct SizeLiterals {
         static let screenWidth: CGFloat = UIScreen.main.bounds.width
         static let screenHeight: CGFloat = UIScreen.main.bounds.height
         static let deviceRatio: CGFloat = screenWidth / screenHeight
+        static let popupWidth = SizeLiterals.Screen.screenWidth * 343 / 375
     }
     
 }
